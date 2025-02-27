d8bfdf8a64abde9ac945f36e4bb66bc217aed3e5
28098 <2809890208@qq.com>
2025-02-27 17:41:03 +0800

    day3

---
+++ lengthOfLastWord_58.py
@@ -0,0 +1,22 @@
+import re
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        space_list = [i.start() for i in re.finditer(" ",s)] #不记也没事不如自己写遍历
+        l=len(space_list)
+        length = len(s)
+        flag=0
+        if l == 0:
+            return length
+        elif space_list[l-1] == length-1:
+            for i in range (l-1, 0 , -1):
+                if space_list[i]-space_list[i-1]!=1:
+                    flag = 1
+                    k = i
+                    break
+            if flag ==1 :
+                return space_list[k]-space_list[k-1]-1
+            else:
+                return space_list[0]
+                
+        else:
+            return (length-1)-space_list[l-1]
