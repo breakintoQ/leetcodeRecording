0e9dae74399717c57769504b63a8b18b0d1093b4
28098 <2809890208@qq.com>
2025-03-01 19:57:01 +0800

    day3

---
+++ strStr_28.py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(0,len(haystack)-len(needle)+1): #最后一个可能的起始位置不会超出 haystack 的长度
+            if haystack[i:i+len(needle)] == needle:  #因为这里在加
+                return i
+        return -1
+
+# kmp两门课看了两遍都是只会思路还看了就忘，嗯啊。暴力到死了属于
+# 这个是官方的暴力，学习一下尽量少的代码解决。
+# 我自己写又得又臭又长了
