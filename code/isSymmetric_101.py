449df31bc29d61b6882c16644ee9736add09a2ca
28098 <2809890208@qq.com>
2025-03-09 20:32:05 +0800

    day9

--- code/isSymmetric_101.py
+++ code/isSymmetric_101.py
@@ -29,8 +29,11 @@ class Solution:  #递归
         if not p or not q:
             return False
         return p.val == q.val and self.check(p.left, q.right) and self.check(p.right, q.left)
+    #三个条件：根相同，左子树的左和右子树的右对称，左子树的右和右子树的左对称
 
     def isSymmetric(self, root: TreeNode) -> bool:
         if not root:
             return True
         return self.check(root.left, root.right)
+    
+    #这个要再看，感觉很奇怪的做法。仔细想想又不奇怪。嗯啊。左子树的左和右子树的右以及左子树的右和右子树的左都应该相同。
